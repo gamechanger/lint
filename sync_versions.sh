8f68332a2c9919d741a98aa52643ca51b45090a0
Miroslav Shubernetskiy <miroslav@miki725.com>
2022-01-20 13:36:16 -0500

    automatically syncing hooks versions with package.json

---
+++ sync_versions.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env sh
+
+HOOKS_FILE=./.pre-commit-hooks.yaml
+PACKAGE_FILE=./package.json
+
+# need to fully consume file to allow usage like:
+# ./sync_versions.sh > ./.pre-commit-hooks.yaml
+# otherwise ./.pre-commit-hooks.yaml is overwritten
+# as empty file since ">" is piped before rading
+# the file in the script
+content=$(< $HOOKS_FILE)
+
+exec &> $HOOKS_FILE
+
+while IFS= read line; do
+    if [ -n "$line" ] && [ -z "${line##*sync:package.json*}" ]; then
+        IFS=' ' read package version < <(
+            echo $line \
+                | awk '{ print $2 }' \
+                | tr -d '"' \
+                | tr -d "'" \
+                | sed -r "s/(.)@/\1 /g"
+        )
+        version_in_package=$(
+            cat $PACKAGE_FILE \
+                | grep "\"$package\"" \
+                | cut -d'"' -f 4
+        )
+        echo "$line" | sed "s/$version/$version_in_package/g"
+    else
+        echo "$line"
+    fi
+done < <(echo "$content")
